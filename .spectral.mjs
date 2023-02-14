c16ad03fda782861b2de8962aa05e4704600b4b3
jamie.williams@yimba.com <jamie.williams@yimba.com>
2023-02-14 15:02:29 +0000

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEzMTYy/spectral.js?branch_slug=master&token=c8a0008b-75ce-4c47-90a9-0214d32a431e";
+export default { extends: ruleset };
